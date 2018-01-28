b0a2f18d92d4bfccb4053a72c812d6eafeafb729
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2018-01-28 15:02:56 -0600

    It was impelmented el provider of react-redux to set the store in the main component

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
 // console.log('Hola mundo!' )
-
+import { Provider } from 'react-redux';
 
 import { createStore } from 'redux';
 
@@ -22,10 +22,13 @@ const store = createStore(
 
 console.log(store.getState());
 
-
 const homeContainer = document.getElementById('home-container')
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
-hydrate( <Home data={data} />, homeContainer);
 
+render(
+  <Provider store={store}>
+    <Home />
+  </Provider>
+, homeContainer);
