a46304c582085e67ff7c106694845c20bc39f93f
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2018-01-29 23:38:14 -0600

    Multipl reducers were implemented

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,22 +1,32 @@
-function data(state, action) {
-    switch (action.type) {
-      case 'SEARCH_VIDEO': {
-        // action.payload.query
-        let results = [];
-        if (action.payload.query) {
-          const list = state.data.categories[2].playlist;
-          results = list.filter((item) => {
-            return item.author.includes(action.payload.query)
-          })
-        }
-        return {
-          ...state,
-          search: results
-        }
+import schema from '../schemas/index.js';
+
+const initialState = {
+  entities: schema.entities,
+  categories: schema.result.categories,
+  search: [],
+}
+
+
+
+function data(state = initialState, action) {
+  switch (action.type) {
+    case 'SEARCH_VIDEO': {
+      // action.payload.query
+      let results = [];
+      if (action.payload.query) {
+        const list = state.data.categories[2].playlist;
+        results = list.filter((item) => {
+          return item.author.includes(action.payload.query)
+        })
       }
-      default:
-        return state
+      return {
+        ...state,
+        search: results
+      }
     }
+    default:
+      return state
   }
-  
-  export default data;
+}
+
+export default data;
