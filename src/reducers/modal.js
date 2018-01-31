9a977b5c4e0c0c2ba1fb32dd23d82d1aa0d09a8c
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2018-01-30 23:10:19 -0600

    We implemented immutable date in the proyect with inmmutablejs

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -1,17 +1,19 @@
-const initialState = {
-    visibility: false,
-    mediaId: null,
+import { fromJS } from 'immutable';
+
+const initialState = fromJS({
+  visibility: false,
+  mediaId: null,
+})
+
+function modal(state = initialState, action) {
+  switch(action.type) {
+    case 'OPEN_MODAL':
+      return state
+    case 'CLOSE_MODAL':
+      return state
+    default:
+      return state
   }
-  
-  function modal(state = initialState, action) {
-    switch(action.type) {
-      case 'OPEN_MODAL':
-        return state
-      case 'CLOSE_MODAL':
-        return state
-      default:
-        return state
-    }
-  }
-  
-  export default modal;
+}
+
+export default modal;
